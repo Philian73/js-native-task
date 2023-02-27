79726eab69e33954a9343edef8ac1a36d3ee3d27
Nikolay Sulteev <Philian73@gmail.com>
2023-02-27 17:22:50 +0400

    Updated chapter7 for js-for-kids

--- #7 Chapter/js/script.js
+++ #7 Chapter/js/script.js
@@ -5,16 +5,29 @@ var words = [
 	["вода", "2/3 массы нашего тела состоит из неё"],
 	["энергетик", "Вредно, но бодрит и вкусно"],
 ]
+/* Генерирую случаный элемент из массива words c 2 элементами внутри
+	0 - загаданное слово
+	1 - подсказка к слову
+*/
 var randomWord = words[Math.floor(Math.random() * words.length)]
+/*
+Создаю пусстой массив, в последствие в котором будет столько же элементов,
+сколько букв в загаданном слове
+*/
 var answerArr = []
+// Создаю переменную, которая будет отвечать за то, сколько букв осталось угадать.
 var remainingLetters = randomWord[0].length
+// Создаю переменную с кол-вом попыток
 var attemptsCount = 6
+// Создаю пустой массив, в последствие в который будут добавляться все введённые буквы
 var allUsedLetters = []
 
+// Цикл, который создаёт в массив answerArr столько элементов, сколько букв в загаданном слове.
 for (var i = 0; i < randomWord[0].length; i++) {
 	answerArr[i] = "_"
 }
 
+// Цикл, который будет повторяться до тех пор, пока не угаданы все буквы слова И пока кол-во попыток > 0;
 while (remainingLetters > 0 && attemptsCount) {
 	var playerLetter = prompt(`Угадайте букву или нажмите "Отмена" для выхода из игры.
 	Подсказка: ${randomWord[1]}.
@@ -30,18 +43,24 @@ while (remainingLetters > 0 && attemptsCount) {
 	} else if (allUsedLetters.includes(playerLetter)) {
 		alert(`Вы уже вводили букву "${playerLetter.toUpperCase()}".`)
 	} else {
+		// Создаю переменную, которая отвечает, ввёл ли игрок правильный символ. Изначально равна false
 		var usedCorrectLetter = false
 
+		// Каждая введённая буква добавляется в общий массив всех введённых букв
 		allUsedLetters.push(playerLetter)
 
 		for (var j = 0; j < randomWord[0].length; j++) {
 			if (randomWord[0][j] === playerLetter.toLowerCase() && answerArr[j] === "_") {
+				// уменьшаю кол-во оставшихся символов
 				remainingLetters--
+				//заменяю "_" в массиве answeArr на угаданную игроком букву
 				answerArr[j] = playerLetter.toLowerCase()
+				// игрок ввёл правильную букву, изменяю false на true, дабы в дальнейшем кол-во оставшихся попыток не убавилось
 				usedCorrectLetter = true
 			}
 		}
 
+		// Кол-во оставшихся попыток должно убавляться только тогда, когда usedCorrectLetter === false
 		if (!usedCorrectLetter) {
 			attemptsCount--
 		}
