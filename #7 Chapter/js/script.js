381147be91e0e9c729fc2a77798d4b6e5cd8cfd9
Nikolay Sulteev <Philian73@gmail.com>
2023-02-27 04:09:59 +0400

    update chater7 for js-for-kids

--- #7 Chapter/js/script.js
+++ #7 Chapter/js/script.js
@@ -2,48 +2,54 @@ var words = [
 	["машина", "На 4 колёсах"],
 	["компьютер", "Любят программисты"],
 	["телефон", "То без чего не может каждый в 21-м веке"],
-	["анал", "То что Даша даст мне"],
+	["вода", "2/3 массы нашего тела состоит из неё"],
 	["энергетик", "Вредно, но бодрит и вкусно"],
 ]
 var randomWord = words[Math.floor(Math.random() * words.length)]
 var answerArr = []
 var remainingLetters = randomWord[0].length
 var attemptsCount = 6
-var userCorrectLetters = []
+var allUsedLetters = []
 
 for (var i = 0; i < randomWord[0].length; i++) {
 	answerArr[i] = "_"
 }
 
-while (remainingLetters > 0 && attemptsCount > 0) {
+while (remainingLetters > 0 && attemptsCount) {
 	var playerLetter = prompt(`Угадайте букву или нажмите "Отмена" для выхода из игры.
-	Подсказка: ${randomWord[1]}
-	Текущий прогресс: ${answerArr.join(" ")}
-	Осталось попыток: ${attemptsCount}`, "")
+	Подсказка: ${randomWord[1]}.
+	Текущий прогресс: ${answerArr.join(" ")}.
+	Осталось попыток: ${attemptsCount}.`, "")
+
 	if (playerLetter === null) {
-		break;
+		break
 	} else if (playerLetter.length !== 1) {
-		alert("Пожалуйста, введите одну БУКВУ.")
-	} else if (userCorrectLetters.includes(playerLetter.toLowerCase())) {
-		alert("Вы уже вводили эту букву. Попробуйте другую.")
+		alert("Пожалуйста, введите ОДНУ БУКВУ!")
+	} else if (!/^[а-яё]+$/i.test(playerLetter)) {
+		alert("Вводить можно только русские буквы!")
+	} else if (allUsedLetters.includes(playerLetter)) {
+		alert(`Вы уже вводили букву "${playerLetter.toUpperCase()}".`)
 	} else {
-		userCorrectLetters.push(playerLetter.toLowerCase())
-		var correctGuess = false;
+		var usedCorrectLetter = false
+
+		allUsedLetters.push(playerLetter)
+
 		for (var j = 0; j < randomWord[0].length; j++) {
 			if (randomWord[0][j] === playerLetter.toLowerCase() && answerArr[j] === "_") {
+				remainingLetters--
 				answerArr[j] = playerLetter.toLowerCase()
-				remainingLetters--;
-				correctGuess = true;
+				usedCorrectLetter = true
 			}
 		}
-		if (!correctGuess) {
-			attemptsCount--;
+
+		if (!usedCorrectLetter) {
+			attemptsCount--
 		}
 	}
 }
 
-if ((answerArr.join("")) === randomWord[0]) {
-	alert(`Молодец! Ты отгадал слово "${randomWord[0][0].toUpperCase() + randomWord[0].slice(1)}".`)
+if (answerArr.join("") === randomWord[0]) {
+	alert(`Молодец! Загаданное слово было: "${randomWord[0][0].toUpperCase() + randomWord[0].slice(1)}".`)
 } else {
 	alert(`Эх, в следующий раз :C
 	Загаданное слово было: "${randomWord[0][0].toUpperCase() + randomWord[0].slice(1)}".`)
