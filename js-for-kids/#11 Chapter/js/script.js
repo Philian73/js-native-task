239af492c9488f2af901da1f25d685599aef8190
Philian73 (Nikolay Sulteev) <Philian73@gmail.com>
2023-03-02 17:07:42 +0400

    Update chapter11 for js-for-kids(finished)

--- js-for-kids/#11 Chapter/js/script.js
+++ js-for-kids/#11 Chapter/js/script.js
@@ -1,33 +1,97 @@
 // Находим картинку с id="map" и присваиваем в переменную
 var map = $('#map')
+// Нажодим кнопку "Режим админа"
+var admin = $("#admin")
 // Находим ширину картинки и присваиваем в переменную
 var width = map[0].clientWidth
 // Находим высоту картинки и присваиваем в переменную
 var height = map[0].clientHeight
 // Изначальное кол-во кликов по карте
 var clicks = 0
+// Изначальное кол-во попыток
+var attemptsCount = 20
+// Запоминаем кол-во попыток, именно из этой переменной мы будем вычитать в дальнейшем
+var thisAt = attemptsCount
 
-// Создаём объект, который содержит случайные координаты X и Y
+// Случайная позиция клада
 var target = {
 	x: getRandomNumber(width),
 	y: getRandomNumber(height),
 }
 
-// Создаём обработчик кликов
+// выводим текст в виде кол-во попыток attemptsCount
+$("#attempts").text(attemptsCount)
+
+// Добавляем обработчик кликов элементу img
 map.click(function (e) {
+	// получаем расстояние от места клика до клада
 	var distance = getDistance(e, target)
+	// преобразуем это расстояние в подсказку
 	var distanceHint = getDistanceHint(distance)
 
+	// увеличиваем кол-во кликов с каждым нажатием
 	clicks++
+	//Вычитаем кол-во попыток
+	thisAt--
+	// обновляем информацию о кол-ве попыток в html
+	$("#attempts").text(thisAt)
 
+	// Если клик был достаточно близко, сообщаем игроку о том, что он победил
 	if (distance < 8) {
 		alert(`Клад найден! Кликов сделано: ${clicks}`)
+		startGame()
+	} else if (!thisAt) {
+		alert("Конце игры! Кликай на карту и снова испытай свою удачу)")
+		startGame()
 	} else {
+		// иначе записываем в элемент #distance подсказку
 		$("#distance").text(distanceHint)
 	}
 })
 
+admin.click(function () {
+	// Выводим сообщение
+	alert("Место где закопан клад теперь показывается на карте!")
+	// Добавляем span с id test
+	$("#game").append('<span id="test"></span>')
+	// Находим span с id test
+	var test = document.querySelector("#test")
+	// Добавляем ему стиль left с x позицией клада
+	test.style.left = target.x + "px"
+	// Добавляем ему стиль top с y позицией клада
+	test.style.top = target.y + "px"
+	// Делаем span видимым
+	test.style.display = "inline-block"
+	// После нажатия на кнопку делаем её неактивной
+	admin[0].disabled = true
+})
 
+
+
+/**
+ * Запускает игру. Обнуляет кол-во кликов, задает новую позицию кладу и т.п.
+ * @date 02.03.2023 - 14:35:43
+ */
+function startGame() {
+	// Возвращаем кол-во попыток на изначальное, указанное в attemptsCount
+	thisAt = attemptsCount
+	// Обновляем текст на изначальное кол-во попыток
+	$("#attempts").text(attemptsCount)
+	// Обновляем текст подсказки
+	$("#distance").text("Тыкни")
+	// Обнуляем кол-во кликов
+	clicks = 0;
+	// Задаём новую позицию кладу
+	target.x = getRandomNumber(width)
+	target.y = getRandomNumber(height)
+	// Отключаем неактивность у кнопки "Режим админа"
+	admin[0].disabled = false
+
+	// Если кнопка была нажата и был показан ответ, удалить ответ
+	if ($("#test")) {
+		$("#test").remove()
+	}
+}
 
 /**
  * Функция генерирует случайное число от 0 до size (не включая его)
@@ -41,12 +105,12 @@ function getRandomNumber(size) {
 }
 
 /**
- * Вычисляет расстояние между e(клик) и target по гиппотинузе
+ * Вычисляет расстояние от клика (e) до клада (target)
  * @date 01.03.2023 - 21:05:20
  *
- * @param {*} e
- * @param {*} target
- * @returns {*}
+ * @param {{}} e
+ * @param {{}} target
+ * @returns {Number} возвращает квадратный корень гиппотинузы
  */
 function getDistance(e, target) {
 	var diffX = e.offsetX - target.x
@@ -56,7 +120,7 @@ function getDistance(e, target) {
 }
 
 /**
- * Отображаем подсказку, насколько близко кликнул игрок по отношению к "кладу"
+ * Получаем строку подсказки для расстояния
  * @date 01.03.2023 - 21:08:01
  *
  * @param {Number} distance в качестве аргумента принимает функцию возвращенное значение getDistance
@@ -64,18 +128,20 @@ function getDistance(e, target) {
  */
 function getDistanceHint(distance) {
 	switch (true) {
-		case (distance < 10):
+		case (distance < 16):
 			return "Обожжёшься!"
-		case (distance < 20):
+		case (distance < 32):
 			return "Очень горячо"
-		case (distance < 40):
+		case (distance < 64):
 			return "Горячо"
-		case (distance < 80):
+		case (distance < 128):
 			return "Тепло"
-		case (distance < 160):
+		case (distance < 256):
 			return "Холодно"
-		case (distance < 320):
+		case (distance < 360):
 			return "Очень холодно"
+		case (distance < 512):
+			return "Очень-очень холодно!"
 		default:
 			return "Замёрзнешь!"
 	}
