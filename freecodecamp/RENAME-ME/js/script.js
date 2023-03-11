22dc159761382f94526142530894966fb30fa0a9
Nikolay Sulteev <Philian73@gmail.com>
2023-03-12 01:32:47 +0400

    Update chapter17 js-for-kids (forEach)

---
+++ freecodecamp/RENAME-ME/js/script.js
@@ -0,0 +1,18 @@
+const people = [
+	{ name: "Николай", age: 25, budget: 40000 },
+	{ name: "Елена", age: 17, budget: 3400 },
+	{ name: "Игорь", age: 49, budget: 50000 },
+	{ name: "Михаил", age: 15, budget: 1800 },
+	{ name: "Василиса", age: 24, budget: 25000 },
+	{ name: "Виктория", age: 38, budget: 2300 },
+]
+
+// for (let i = 0; i < people.length; i++) {
+// 	console.log(people[i])
+// }
+
+// people.forEach((person) => console.log(person))
+
+// for (let person of people) {
+// 	console.log(person)
+// }
