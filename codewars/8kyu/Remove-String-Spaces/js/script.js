94f0a1a78a1675f132115d18c3232921d51db1b3
Nikolay Sulteev <Philian73@gmail.com>
2023-03-14 00:14:33 +0400

    Add tusk "Remove String Spaces" for codewars 8KYU

---
+++ codewars/8kyu/Remove-String-Spaces/js/script.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+const noSpace = x => x.replace(/\s/g, '')
+
+console.log(noSpace("asdas das das asd asd asdsad")) // asdasdasdasasdasdasdsad
