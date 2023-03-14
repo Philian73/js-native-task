2b5fd89b4c33d917e623809185f6180d1318f743
Nikolay Sulteev <Philian73@gmail.com>
2023-03-14 07:53:28 +0400

    add tusk10 for BasicAlgorithm freecodecamp

---
+++ freecodecamp/Basic-Algorithm-Scripting/10/js/script.js
@@ -0,0 +1,5 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/boo-who
+
+const booWho = bool => typeof bool === 'boolean'
+
+console.log(booWho(null))
