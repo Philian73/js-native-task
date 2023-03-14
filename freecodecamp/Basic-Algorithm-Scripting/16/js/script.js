29350c6a962ee37ec080ff6dd4d467640253b054
Nikolay Sulteev <Philian73@gmail.com>
2023-03-15 01:08:26 +0400

    Add tusk16 for BasicAlgorithm freecodecamp

---
+++ freecodecamp/Basic-Algorithm-Scripting/16/js/script.js
@@ -0,0 +1,14 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/chunky-monkey
+
+function chunkArrayInGroups(arr, size) {
+	const newArr = []
+
+	for (let i = 0; i < arr.length; i += size) {
+		newArr.push(arr.slice(i, i + size))
+	}
+
+	return newArr
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2))
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
